3586e9e3eaf982592ce737c508da73159c37fe4d
HadeelAlabadi <163149057+HadeelAlabadi@users.noreply.github.com>
2024-09-09 14:03:33 +0300

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,52 @@
+/*var
+h=2,
+j=8,
+k=1,
+s=h+j+k;
+console.log(s);
+var id305=["hadeel","21","jordan"];
+ alert("my name: " +id305[0]);
+
+ var class7c=["hala","saja","raya","miss","hadeel"];
+ alert("the number of class="+class7c.length);
+
+ var rase={first:"ahmad",second:"aseel",thired:"ali",fourth:"hadeel"};
+ alert("NAME: "+ rase.first);*/
+ /// if stetment////////////
+/*
+ var i=0, studant=[30,0,10,50,90,80,88];
+ if (studant[4]>=50) {
+   console.log("ناجح");
+    
+ } else {
+    console.log("راسب");
+    
+ }
+ */
+
+ ////switch/////////
+ /*
+ var day;
+ switch (new Date().getDay()) {  // استخدم New Date() بدلاً من New Date
+     case 0: day = "Sunday"; break;
+     case 1: day = "Monday"; break;
+     case 2: day = "Tuesday"; break;
+     case 3: day = "Wednesday"; break;
+     case 4: day = "Thursday"; break;
+     case 5: day = "Friday"; break;
+     case 6: day = "Saturday"; break;
+ }
+ alert("Today is: " + day);
+ 
+ var i = 0, studant = ["hala","saja","raya","miss","hadeel"];
+
+ while (i < studant.length) {
+     document.getElementById("text").innerHTML += studant[i] + "<br> "; // إضافة مسافة لفصل الأرقام
+     i++;
+ }
+ */
+
+ document.write("hello everyone ");
+
+ 
+ 
---
+++ day2.js
@@ -0,0 +1,57 @@
+/*let a="we \"love\" \n ";
+let b="\n javascript";
+document.write(a+b);
+*/
+/*//concatenation
+let a= "we love";
+let b="javascript";
+document.write(a+" "+b);*/
+
+/*//template string
+let a="we love";
+let b="javascript";
+let c="and";
+let d="programming";
+console.log(a+" "+b+" "+c+" "+d);
+console.log(`${a} ${b} ${c} ${d}`);
+/////////////////////////
+let title="jaascript";
+let desc="welcome";
+let mackup=`<div class="title">
+    <div class="chiled">
+        <h1>${title}</h1>
+        <p>${desc}</p>
+    </div>
+</div>`;
+document.write(mackup);
+
+
+*/
+
+/*
+//challenge
+
+let title_article = "Elzero",
+description_article = "Elzero web school",
+date_article="25/10";
+
+let contins =`<div class="contins">
+        <h3>${title_article}</h3>
+        <p>${description_article}</p>
+        <span>${date_article}</span>
+    </div>
+`;
+document.write(contins.repeat(4));
+
+
+
+
+*/
+
+
+
+
+
+
+
+
